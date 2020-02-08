5b6d1692a49776f6e0a2e6094214a41678eeb5ac
gmai2006 <gmai2006@gmail.com>
2020-02-08 08:41:50 -0800

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,5 +1,5 @@
-# PowerBuilder pbl file extraction
-This program extracts source code from PowerBuilder pbl files
+#PowerBuilder pbl file extraction
+This program extracts the PB source code from PowerBuilder pbl files.
 
 #Instruction
 To extract source from PowerBuilder pbl files
